4d66cd35fe1aa3a7dace246c5da1ade2b9b59588
Joao Parreira <joao_parreira@mail.telepac.pt>
2024-11-24 00:05:24 +0000

    add App Controller & ConnStr

---
+++ somiod/Controllers/ApplicationsController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace somiod.Controllers
+{
+    [RoutePrefix("api/applications")]
+    public class ApplicationsController : ApiController
+    {
+        string connectionString = Properties.Settings.Default.ConnStr;
+    }
+}
