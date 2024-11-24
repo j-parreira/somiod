22578aaa02f89fe6bebcbd2bd23f868a71fb6767
Diogo Abegão <2222184@my.ipleiria.pt>
2024-11-24 18:36:44 +0000

    update test_insert to fit the test case

--- somiod/DBScripts/insert_test_tables.sql
+++ somiod/DBScripts/insert_test_tables.sql
@@ -1,27 +1,15 @@
-﻿INSERT INTO Applications ([Name], [Creation_DateTime])
+﻿INSERT INTO Applications ([Name], [Creation_DateTime]) 
 VALUES 
-    ('Record', GETDATE()),
-    ('Diario Leiria', GETDATE()),
-    ('Maria', GETDATE());
+    ('Lighting', GETDATE());
 
-INSERT INTO Containers ([Name], [Creation_DateTime], [Parent])
+INSERT INTO Containers ([Name], [Creation_DateTime], [Parent]) 
 VALUES 
-    ('Futebol', GETDATE(), 1), 
-    ('Basquetebol', GETDATE(), 1), 
-    ('Cultura', GETDATE(), 2), 
-    ('Moda', GETDATE(), 3);
+    ('light_bulb', GETDATE(), 1);
 
-INSERT INTO Records ([Name], [Content], [Creation_DateTime], [Parent])
+INSERT INTO Records ([Name], [Content], [Creation_DateTime], [Parent]) 
 VALUES 
-    ('Benfica', 'Sample content for Benfica', GETDATE(), 1), 
-    ('Sporting', 'Sample content for Sporting', GETDATE(), 1), 
-    ('Musica', 'Sample content for Musica', GETDATE(), 2), 
-    ('Lisboa', 'Sample content for Lisboa', GETDATE(), 3);
+    ('ON', 'Light is turned on', GETDATE(), 1);
 
-INSERT INTO Notifications ([Name], [Creation_DateTime], [Parent], [Event], [Endpoint])
+INSERT INTO Notifications ([Name], [Creation_DateTime], [Parent], [Event], [Endpoint]) 
 VALUES 
-    ('Notification 1', GETDATE(), 1, '1', 'http://localhost:5000/api/somiod/Record/Futebol/Benfica/creation'),
-    ('Notification 2', GETDATE(), 1, '1', 'http://localhost:5000/api/somiod/Record/Futebol/Sporting/creation'), 
-    ('Notification 3', GETDATE(), 2, '2', 'http://localhost:5000/api/somiod/DiarioLeiria/Cultura/Musica/creation'),
-    ('Notification 4', GETDATE(), 3, '2', 'http://localhost:5000/api/somiod/Maria/Moda/Lisboa/creation'),
-    ('Notification 5', GETDATE(), 1, '2', 'http://localhost:5000/api/somiod/Record/Futebol/Sporting/deletion');
+    ('Notification 1', GETDATE(), 1, '1', 'http://127.0.0.1');
